c63819559fb972e7128793685d604bca77703f7c
Jonas Jordan <jj@sernet.de>
2023-05-31 10:14:09 +0200

    chore(deps): update plugin org.springframework.boot to v3.1.0
    VEO-1116

--- src/main/kotlin/org/veo/accounts/ExceptionHandler.kt
+++ src/main/kotlin/org/veo/accounts/ExceptionHandler.kt
@@ -15,6 +15,9 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+// TODO stop using MissingKotlinParameterException
+@file:Suppress("DEPRECATION")
+
 package org.veo.accounts
 
 import com.fasterxml.jackson.core.JsonParseException
