e900895f789b730d04e7dfbceaf60b852327ae96
Jonas Jordan <jj@sernet.de>
2023-11-06 15:24:03 +0100

    move single line comment after license header
    verinice-veo#1116

--- src/main/kotlin/org/veo/accounts/ExceptionHandler.kt
+++ src/main/kotlin/org/veo/accounts/ExceptionHandler.kt
@@ -15,8 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-// TODO stop using MissingKotlinParameterException
-@file:Suppress("DEPRECATION")
+@file:Suppress("DEPRECATION") // TODO stop using MissingKotlinParameterException
 
 package org.veo.accounts
 
