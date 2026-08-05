73195309aecf80ac7ccfb43be46e1802f4d56158
Jonas Jordan <jj@sernet.de>
2026-08-05 12:56:49 +0200

    update netty to 4.2.16

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,9 +48,9 @@ configurations.all {
             useVersion("11.0.22")
             because("Security fixes")
         }
-        if (requested.group == "io.netty" && requested.version == "4.2.12.Final") {
-            useVersion("4.2.13.Final")
-            because("Security fixes")
+        if (requested.group == "io.netty" && requested.version == "4.2.15.Final") {
+            useVersion("4.2.16.Final")
+            because("CVE-2026-5990")
         }
     }
 }
